3c161c5632697b8ddbfbea9fc460f580d929a1a2
selikem1 <hkamuzu001@st.ug.edu.gh>
2024-05-27 08:45:06 -0400

    change background

---
+++ my-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My app My name is Amuzu Hope Kwabla!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#add8e6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
