49bb1509a26c1b4d7ff7a3bf3d96cfb955bb0c37
selikem1 <hkamuzu001@st.ug.edu.gh>
2024-05-27 09:31:37 -0400

    changge fontsize and bold name

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My app My name is Amuzu Hope Kwabla!</Text>
+      <Text style={styles.mainText}>My app My name is <Text style={styles.subText}>Amuzu Hope Kwabla!</Text> </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
-    fontSize: '24'
   },
+  mainText:{
+    fontSize: 24,
+  },
+  subText:{
+    fontWeight: 'bold'
+  }
 });
